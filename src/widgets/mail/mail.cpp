642f188e80529af78c6758f683a7f7237f24ba40
moukito <theofremont50@gmail.com>
2024-02-24 16:47:50 +0100

    Setting some small test.

--- src/widgets/mail/mail.cpp
+++ src/widgets/mail/mail.cpp
@@ -1,13 +1,25 @@
 #include "mail.h"
 
 Mail::Mail(QWidget *parent)
-    : QWidget(parent)
-{
+        : QWidget(parent) {
     lay = new QHBoxLayout(this);
-    
+
     lay->setContentsMargins(0, 0, 0, 0);
     lay->setSpacing(0);
 
     setProperty("class", "mail");
     setAttribute(Qt::WA_StyledBackground);
+
+    QByteArray login{""};
+    QByteArray password{""};
+
+    mImap = new ImapConnection(login, password, this);
+
+    connect(mImap, &ImapConnection::accessRead, this, &Mail::test);
+
+    mImap->sendRequest("select INBOX");
+}
+
+void Mail::test() {
+    qDebug() << mImap->getData();
 }
